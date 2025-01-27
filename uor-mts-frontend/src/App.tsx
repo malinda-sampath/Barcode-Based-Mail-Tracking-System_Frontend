7c1e9efaca83e8ce264dcd1ea2549c2b79c8180b
shashikalaayeshani86 <shashikalaayeshani86@gmail.com>
2025-01-27 20:56:10 +0530

    CLF 0003

--- uor-mts-frontend/src/App.tsx
+++ uor-mts-frontend/src/App.tsx
@@ -1,22 +1,12 @@
 import React from "react";
 import "./App.css";
+import Button from "./components/buttonComponents/Button";
+import AllButtonComponents from "./components/buttonComponents/AllButtonComponents";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AllButtonComponents/>
     </div>
   );
 }
