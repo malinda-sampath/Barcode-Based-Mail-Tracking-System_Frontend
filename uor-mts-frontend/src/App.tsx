7c4affaf8b072f4acb57338f81125d1f4c8594d3
malinda-sampath <malindasampath45@gmail.com>
2025-02-07 09:19:36 +0530

    Resolve a merge conflict

--- uor-mts-frontend/src/App.tsx
+++ uor-mts-frontend/src/App.tsx
@@ -2,23 +2,16 @@ import React from "react";
 
 import TableTest from "./components/table/TableTest";
 import AppRouter from "./routes/AppRouter";
-<<<<<<< HEAD
 import Form from "./components/form/Form";
-=======
 import AllButtonComponents from "./components/buttonComponents/AllButtonComponents";
->>>>>>> b3eba5ec396832b94f767fae522e6a4289c49373
 
 const App: React.FC = () => (
   <div>
     {/* <TableTest /> */}
-    {/* <AppRouter /> */}
-<<<<<<< HEAD
-   <Form/>
-=======
-    <AllButtonComponents/>
->>>>>>> b3eba5ec396832b94f767fae522e6a4289c49373
+    <AppRouter />
+    {/* <Form />
+    <AllButtonComponents /> */}
   </div>
 );
-
 
 export default App;
