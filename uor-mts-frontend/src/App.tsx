c2b33abf6d389392103542de099307e134abf6b4
Gayan Dissanayake <gayandissanayake20@gmail.com>
2025-02-13 01:18:59 +0530

    create Login page interface

--- uor-mts-frontend/src/App.tsx
+++ uor-mts-frontend/src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import TableTest from "./components/table/TableTest";
 import AppRouter from "./routes/AppRouter";
 import LoginL from "./pages/LoginL";
+import LoginG from "./pages/LoginG"
 
 const App: React.FC = () => (
   <div>
     {/* <TableTest /> */}
     {/* <AppRouter /> */}
-    <LoginL />
+    <LoginG/>
+
 
   </div>
 );
--- uor-mts-frontend/tailwind.config.js
+++ uor-mts-frontend/tailwind.config.js
@@ -5,3 +5,15 @@ module.exports = {
   },
   plugins: [],
 };
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'custom-bg': "url('assets/uorbg.jpg')", // Add your image path
+      },
+    },
+  },
+  plugins: [],
+};
