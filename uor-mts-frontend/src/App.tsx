cfeb458a8e62ef4b48622e9942e806868421e5c4
malinda-sampath <malindasampath45@gmail.com>
2025-02-13 12:35:39 +0530

    CLF_0011

--- uor-mts-frontend/src/App.tsx
+++ uor-mts-frontend/src/App.tsx
@@ -6,7 +6,7 @@ import TrackingL from "./pages/TrackingL";
 const App: React.FC = () => (
   <div>
     <TrackingG /> //This will render the TrackingG component(For Gayan)
-    <TrackingL /> //This will render the TrackingG component(For Gayan)
+    <TrackingL /> //This will render the TrackingL component(For Layan)
   </div>
 );
 
