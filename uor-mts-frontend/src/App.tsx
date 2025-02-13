4a7e2bf571a0793bc3df4b493796771390e1d9e8
Layanrasanga <layanrasan@gmail.com>
2025-02-13 22:30:55 +0530

    CLF_0011

--- uor-mts-frontend/src/App.tsx
+++ uor-mts-frontend/src/App.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import TrackingG from "./pages/TrackingG";
 import TrackingL from "./pages/TrackingL";
 
 
 const App: React.FC = () => (
   <div>
-    <TrackingG /> //This will render the TrackingG component(For Gayan)
+    
     <TrackingL /> //This will render the TrackingL component(For Layan)
   </div>
 );
