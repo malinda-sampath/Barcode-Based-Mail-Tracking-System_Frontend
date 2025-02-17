2286cb7397ec530adfd5c521ad7d9d2506927ef4
Manuri-Rasara <manurirasarahewage@gmail.com>
2025-02-17 19:43:20 +0530

    CLF_0004_ii

--- uor-mts-frontend/src/pages/AdminManagement.tsx
+++ uor-mts-frontend/src/pages/AdminManagement.tsx
@@ -1,5 +1,6 @@
+import { AdminTable } from "../PagesComponents/dashboard/Table/AdminTable";
 import React from "react";
 
 export default function AdminManagement() {
-  return <div className="m-12">This is Admin Management Page</div>;
+  return <div className="m-12"><AdminTable></AdminTable></div>;
 }
