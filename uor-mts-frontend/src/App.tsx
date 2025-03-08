c6573b996f31fde1cb4a56c642941e99b2ae349e
malinda-sampath <malindasampath45@gmail.com>
2025-03-08 17:12:46 +0530

    CLF_0012-

--- uor-mts-frontend/src/App.tsx
+++ uor-mts-frontend/src/App.tsx
@@ -2,27 +2,11 @@ import React from "react";
 import TrackingL from "./pages/TrackingL";
 
 const App: React.FC = () => {
-  // Define the onClick handler function
-  const handleClick = () => {
-    alert("Button clicked!");
-  };
-
-<<<<<<< HEAD
   return (
-    
-      
-      <TrackingG></TrackingG>
-      
-   
+    <div>
+      <TrackingL />
+    </div>
   );
 };
-=======
-const App: React.FC = () => (
-  <div>
-    
-    <TrackingL /> //This will render the TrackingL component(For Layan)
-  </div>
-);
->>>>>>> 4a7e2bf571a0793bc3df4b493796771390e1d9e8
 
 export default App;
