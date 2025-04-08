23a7f1233da3b5d74a131340d753accb57693175
Malinda Sampath <167544354+malinda-sampath@users.noreply.github.com>
2025-04-09 01:58:44 +0530

    Revert "Develop"

--- uor-mts-frontend/src/pages/AllMails.tsx
+++ uor-mts-frontend/src/pages/AllMails.tsx
@@ -23,7 +23,7 @@ const mailData: MailData[] = [
   { mailId: "M03", sender: "Sam Wilson", recipient: "Invoice Details" },
 ];
 
-const columns: { key: keyof MailData; label: string }[] = [
+const columns : { key: keyof MailData; label: string } []=[
   { key: "mailId", label: "MailId" },
   { key: "sender", label: "Sender" },
   { key: "recipient", label: "Recipient" },
@@ -46,39 +46,39 @@ export default function AllMails() {
   };
 
   return (
-    <div className="ml-4 sm:ml-8 md:ml-16 px-4 sm:px-6 lg:px-8">
-      <h1 className="text-xl sm:text-2xl font-semibold mt-2 text-[#611010]">
-        All Mails
-      </h1>
-      <p className="text-xs sm:text-sm text-gray-500 ">{currentDate}</p>
-
-      <div className="bg-white p-6 rounded-lg shadow-md mt-6">
-        <div className="my-16 mx-28">
-          <AllMailsTable<MailData> // Explicitly specify MailData type for Table component
-            columns={columns}
-            data={mailData}
-            onViewClick={handleViewClick}
-            onDeleteClick={handleDeleteClick}
-            hideEditButton={true}
-            searchableKeys={["sender", "recipient"]}
-          />
-        </div>
-
-        {showViewPopup && selectedMail && (
-          <ViewPopup onClose={() => setShowViewPopup(false)} />
-        )}
-
-        {showDeletePopup && deleteMailId && (
-          <DeleteConfirmation
-            mailId={deleteMailId}
-            onConfirm={() => {
-              console.log("Mail deleted:", deleteMailId);
-              setShowDeletePopup(false);
-            }}
-            onCancel={() => setShowDeletePopup(false)}
-          />
-        )}
+    <div>
+      <div className="absolute top-0 px-4 ml-4 sm:ml-8 md:ml-16 sm:px-6 lg:px-8">
+        <h1 className="text-xl sm:text-2xl font-semibold mt-2 text-[#611010]">
+          All Mails
+        </h1>
+        <p className="text-xs text-gray-500 sm:text-sm">{currentDate}</p>
       </div>
+
+      <div className="my-16 mx-28">
+        <AllMailsTable<MailData> // Explicitly specify MailData type for Table component
+          columns={columns}
+          data={mailData}
+          onViewClick={handleViewClick}
+          onDeleteClick={handleDeleteClick}
+          hideEditButton={true}
+          searchableKeys={["sender", "recipient"]}
+        />
+      </div>
+
+      {showViewPopup && selectedMail && (
+        <ViewPopup onClose={() => setShowViewPopup(false)} />
+      )}
+
+      {showDeletePopup && deleteMailId && (
+        <DeleteConfirmation
+          mailId={deleteMailId}
+          onConfirm={() => {
+            console.log("Mail deleted:", deleteMailId);
+            setShowDeletePopup(false);
+          }}
+          onCancel={() => setShowDeletePopup(false)}
+        />
+      )}
     </div>
   );
 }
